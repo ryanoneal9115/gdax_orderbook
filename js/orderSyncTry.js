871bbb813ccef2dd8ae3a0e1ef94d6f4ac84f909
TheStoof <neyens.s@gmail.com>
2017-06-24 15:03:59 -0400

    Techan + Historical Data Charting
    Added the capability to graph historical data using Techan. Currently draws all three coin charts and does not filter.

--- js/orderSyncTry.js
+++ js/orderSyncTry.js
@@ -4,6 +4,8 @@ var WebsocketClient = Gdax.WebsocketClient;
 var PublicClient = Gdax.PublicClient;
 var Orderbook = Gdax.Orderbook;
 var util = require('util');
+var d3 = require("d3");
+var techan = require("techan");
 var _ = {
   forEach: require('lodash.foreach'),
   assign: require('lodash.assign'),
@@ -22,6 +24,11 @@ var OrderbookSync = function(productID, apiURI, websocketURI, authenticatedClien
 
   WebsocketClient.call(self, self.productID, self.websocketURI);
   self.loadOrderbook();
+
+  (function() {
+      self.loadHistoricalData();
+      //setInterval(self.loadHistoricalData, 20000);
+   })();
 };
 
 util.inherits(OrderbookSync, WebsocketClient);
@@ -41,6 +48,189 @@ _.assign(OrderbookSync.prototype, new function() {
       self.processMessage(data);
     }
   };
+  prototype.populateHistoricalData = function(err, response, body){
+    if (err) {
+        throw 'Failed to load history: ' + err;
+      }
+
+      if (response.statusCode !== 200) {
+          throw 'Failed to load orderbook: ' + response.statusCode;
+      }
+      var data = JSON.parse(response.body);
+
+
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 50},
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
+
+        var convertTimeToDate = function(d) {
+          var time = new Date(d * 1000);
+          return time;
+        };
+
+        var parseDate = d3.timeParse("%d-%b-%y");
+
+        var x = techan.scale.financetime()
+            .range([0, width]);
+
+    var y = d3.scaleLinear()
+            .range([height, 0]);
+
+    var candlestick = techan.plot.candlestick()
+            .xScale(x)
+            .yScale(y);
+
+    var ichimoku = techan.plot.ichimoku()
+            .xScale(x)
+            .yScale(y);
+
+    var xAxis = d3.axisBottom(x);
+
+    var yAxis = d3.axisLeft(y)
+            .tickFormat(d3.format(",.3s"));
+
+    var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("clipPath")
+            .attr("id", "clip")
+        .append("rect")
+            .attr("x", 0)
+            .attr("y", y(1))
+            .attr("width", width)
+            .attr("height", y(0) - y(1));
+
+    var ichimokuIndicator = techan.indicator.ichimoku();
+    // Don't show where indicators don't have data
+    var indicatorPreRoll = ichimokuIndicator.kijunSen()+ichimokuIndicator.senkouSpanB();
+
+    var accessor = candlestick.accessor();
+    data = data.map(function(d) {
+          var time = convertTimeToDate(d[0]);
+          return {
+              date: time,
+              open: +d[3],
+              high: +d[2],
+              low: +d[1],
+              close: +d[4],
+              volume: +d[5]
+          };
+      }).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
+
+      svg.append("g")
+                .attr("class", "ichimoku")
+                .attr("clip-path", "url(#clip)");
+
+        svg.append("g")
+                .attr("class", "candlestick")
+                .attr("clip-path", "url(#clip)");
+
+        svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")");
+
+        svg.append("g")
+                .attr("class", "y axis")
+            .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("Ichimoku");
+
+        // Data to display initially
+        draw(data.slice(0, data.length-20));
+        // Only want this button to be active if the data has loaded
+        d3.select("button").on("click", function() { draw(data); }).style("display", "inline");
+
+    function draw(data) {
+        var ichimokuData = ichimokuIndicator(data);
+        x.domain(data.map(ichimokuIndicator.accessor().d));
+        // Calculate the y domain for visible data points (ensure to include Kijun Sen additional data offset)
+        y.domain(techan.scale.plot.ichimoku(ichimokuData.slice(indicatorPreRoll-ichimokuIndicator.kijunSen())).domain());
+
+        // Logic to ensure that at least +KijunSen displacement is applied to display cloud plotted ahead of ohlc
+        x.zoomable().clamp(false).domain([indicatorPreRoll, data.length+ichimokuIndicator.kijunSen()]);
+
+        svg.selectAll("g.candlestick").datum(data).call(candlestick);
+        svg.selectAll("g.ichimoku").datum(ichimokuData).call(ichimoku);
+        svg.selectAll("g.x.axis").call(xAxis);
+        svg.selectAll("g.y.axis").call(yAxis);
+    }
+        // var x = techan.scale.financetime()
+        //         .range([0, width]);
+        //
+        // var y = d3.scaleLinear()
+        //         .range([height, 0]);
+        //
+        // var candlestick = techan.plot.candlestick()
+        //         .xScale(x)
+        //         .yScale(y);
+        //
+        // var xAxis = d3.axisBottom()
+        //         .scale(x);
+        //
+        // var yAxis = d3.axisLeft()
+        //         .scale(y);
+        //
+        // var svg = d3.select("#chart").append("svg")
+        //         .attr("width", width + margin.left + margin.right)
+        //         .attr("height", height + margin.top + margin.bottom)
+        //         .append("g")
+        //         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        //
+        //   var accessor = candlestick.accessor();
+        //
+        //   data = data.map(function(d) {
+        //       var time = convertTimeToDate(d[0]);
+        //       return {
+        //           date: time,
+        //           open: +d[3],
+        //           high: +d[2],
+        //           low: +d[1],
+        //           close: +d[4],
+        //           volume: +d[5]
+        //       };
+        //   }).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
+        //
+        //   svg.append("g")
+        //           .attr("class", "candlestick");
+        //
+        //   svg.append("g")
+        //           .attr("class", "x axis")
+        //           .attr("transform", "translate(0," + height + ")");
+        //
+        //   svg.append("g")
+        //           .attr("class", "y axis")
+        //           .append("text")
+        //           .attr("transform", "rotate(-90)")
+        //           .attr("y", 6)
+        //           .attr("dy", ".71em")
+        //           .style("text-anchor", "end")
+        //           .text("Price ($)");
+        //
+        //   // Data to display initially
+        //   draw(data.slice(0, data.length-20));
+        //   // Only want this button to be active if the data has loaded
+        //   d3.select("button").on("click", function() { draw(data); }).style("display", "inline");
+        //
+        // function draw(data) {
+        //     x.domain(data.map(candlestick.accessor().d));
+        //     y.domain(techan.scale.plot.ohlc(data, candlestick.accessor()).domain());
+        //
+        //     svg.selectAll("g.candlestick").datum(data).call(candlestick);
+        //     svg.selectAll("g.x.axis").call(xAxis);
+        //     svg.selectAll("g.y.axis").call(yAxis);
+        // }
+  }
+  prototype.loadHistoricalData = function() {
+      var self = this;
+      self.publicClient.getProductHistoricRates(self.populateHistoricalData);
+  }
 
   prototype.loadOrderbook = function() {
     var self = this;
@@ -157,6 +347,7 @@ function populateTradeHistory(data){
 		//pop the top
 		tradeHistory.shift();
 	}
+
 	tradeHistory.push(data);
 	refreshTradeHistory();
 }
@@ -300,6 +491,11 @@ function callPopulateBids(){
 function callPopulateAsks(){
 	populateAsks();
 	setTimeout(callPopulateAsks, 500);
+}
+
+function updateCharts() {
+  alert();
+  //OrderbookSync.prototype.loadHistoricalData();
 }
 
 setTimeout(callPopulateAsks, 500);
